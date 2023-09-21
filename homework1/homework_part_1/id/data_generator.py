1f821c4f83e832abd0dd46cb56428b651a8dd845
Songblade <sgreenga@mail.yu.edu>
2023-09-20 21:31:24 -0400

    I changed the formatting of the files and added a data_generator.

---
+++ homework1/homework_part_1/id/data_generator.py
@@ -0,0 +1,33 @@
+import search
+
+
+def run_test(side_length):
+    print("Running for a side length of " + str(side_length))
+
+    total_insert, total_delete, total_depth, total_time, total_timeouts = 0, 0, 0, 0, 0
+    denominator = 100  # the number of times we run the program
+    for x in range(denominator):
+        results = search.search(side_length)
+
+        total_depth += len(results[0][1])
+        total_insert += results[1]
+        total_delete += results[2]
+        total_time += results[3]
+        if results[3] == 100:
+            total_timeouts += 1  # total_timeouts tallies how many times the algorithm times out
+
+    average_insert = total_insert / denominator
+    average_delete = total_delete / denominator
+    average_depth = total_depth / denominator
+    average_time = total_time / denominator
+
+    print("Average depth: " + str(average_depth))
+    print("Average inserts: " + str(average_insert))
+    print("Average removes: " + str(average_delete))
+    print("Average time in seconds: " + str(average_time))
+    print("Number of timeouts: " + str(total_timeouts) + "\n")
+
+
+run_test(2)
+run_test(3)
+run_test(4)
--- homework1/homework_part_1/id/stack.py
+++ homework1/homework_part_1/id/stack.py
@@ -4,24 +4,27 @@ empty stack [0]
 3 is the top of the stack
 '''
 
-# creates a stack with x
 
+# creates a stack with x
 def create(x):
-    s=[0]
-    insert(s,x)
+    s = [0]
+    insert(s, x)
     return s    
 
-def is_empty(s):
-    return s==[0]
 
-def insert(s,x):
-    s[0]=[x,s[0]]
+def is_empty(s):
+    return s == [0]
+
+
+def insert(s, x):
+    s[0] = [x, s[0]]
+
 
 def remove(s):
-    if is_empty(s): # underflow
+    if is_empty(s):  # underflow
         return 0
-    x=s[0][0]
-    s[0]=s[0][1]
+    x = s[0][0]
+    s[0] = s[0][1]
     return x
 
 

4f89c06ed600dec7afc2849231079661065a1565
Songblade <sgreenga@mail.yu.edu>
2023-09-21 22:22:35 -0400

    I created my data generator for homework 2 and cleaned up the files.

--- homework1/homework_part_2/search.py
+++ homework1/homework_part_2/search.py
@@ -3,20 +3,21 @@
 import state
 import frontier
 
+
 def search(n):
-    s=state.create(n)
-    print(s)
-    f=frontier.create(s)
+    s = state.create(n)
+    # print(s)
+    f = frontier.create(s)
     while not frontier.is_empty(f):
-        s=frontier.remove(f)
+        s = frontier.remove(f)
         if state.is_target(s):
             return [s, f[1]]
-        ns=state.get_next(s)
+        ns = state.get_next(s)
         for i in ns:
-            frontier.insert(f,i)
+            frontier.insert(f, i)
     return 0
 
-print (search(3))
+# print (search(3))
 
 
 
